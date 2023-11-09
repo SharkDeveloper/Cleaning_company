037100888fe9d55422c49ae721e39b5931281e7e
MikhailAG <mikhail.gubanov.a@gmail.com>
2023-11-09 21:49:56 +0300

    urls views and html pages

--- cleaning_company/main/views.py
+++ cleaning_company/main/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+
+
 def index(request):
     return render(request, 'index.html')
+
+
+def about(request):
+    return render(request, 'about.html')
+
+
+def account(request):
+    return render(request, 'account.html')
+
+
+def services(request):
+    return render(request, 'services.html')
