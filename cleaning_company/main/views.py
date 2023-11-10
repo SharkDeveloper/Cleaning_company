d1ee71326c6f0513d2849c625c7afdb893953510
MikhailAG <mikhail.gubanov.a@gmail.com>
2023-11-10 05:32:05 +0300

    bug fix

--- cleaning_company/main/views.py
+++ cleaning_company/main/views.py
@@ -18,34 +18,34 @@ def about(request):
 
 
 def account(request):
-    try:
-        if not isinstance(settings.CURRENT_USER, str):
-            contex = {
-                "current-user": settings.CURRENT_USER
+    if request.method == 'POST' and 'login-app' in request.POST:
+        user = request.POST['login']
+        password = request.POST['password']
+        print(user, password)
+        if Clients.objects.filter(login=user, password=password).first():
+            settings.CURRENT_USER = Clients.objects.filter(login=user, password=password).first()
+            context = {
+                "current_user": settings.CURRENT_USER
             }
-            return render(request, 'account.html', context=contex)
-        if request.method == 'POST' and 'login-app' in request.POST:
-
-            user = request.POST['login']
-            password = request.POST['password']
-            if Clients.objects.filter(login=user, password=password).first():
-                settings.CURRENT_USER = Clients.objects.filter(name=user, password=password).first()
-                contex = {
-                    "current-user": settings.CURRENT_USER
-                }
-                return render(request, 'account.html', context=contex)
-        if request.method == 'POST' and 'reg-app' in request.POST:
-            client = Clients()
-            client.login = request.POST["login"]
-            client.email = request.POST["email"]
-            client.phone = request.POST["phone number"]
-            client.password = request.POST["password"]
-            client.name = request.POST["username"]
-            client.save()
-    except:
+            print(settings.CURRENT_USER.name)
+            return render(request, 'account.html', context)
+    if request.method == 'POST' and 'reg-app' in request.POST:
+        client = Clients()
+        client.login = request.POST["login"]
+        client.email = request.POST["email"]
+        client.phone = request.POST["phone number"]
+        client.password = request.POST["password"]
+        client.name = request.POST["username"]
+        client.save()
+    if not isinstance(settings.CURRENT_USER, str):
+        context = {
+            "current_user": settings.CURRENT_USER
+        }
+        print(settings.CURRENT_USER.name)
+        return render(request, 'account.html', context)
+    else:
+        print('penis')
         return render(request, 'auth.html')
-    return render(request, 'auth.html')
-
 
 def services(request):
     if request.method == 'POST' and 'get-services' in request.POST:
