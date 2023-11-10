c7a9822bb195a50d5df5b5f4f0e9b81f07cef892
Valerian <pvalera@gmail.com>
2023-11-10 05:08:31 +0300

    Точной последний

--- cleaning_company/main/views.py
+++ cleaning_company/main/views.py
@@ -18,31 +18,32 @@ def about(request):
 
 
 def account(request):
-    if not isinstance(settings.CURRENT_USER, str):
-        contex = {
-                "current-user":settings.CURRENT_USER
-            }
-        return render(request, 'account.html', context=contex )
-    if request.method == 'POST' and 'login-app' in request.POST:
-        
-        user = request.POST['login']
-        password = request.POST['password']
-        if Clients.objects.filter(login=user, password=password).first():
-            settings.CURRENT_USER = Clients.objects.filter(name=user, password=password).first()
+    try:
+        if not isinstance(settings.CURRENT_USER, str):
             contex = {
-                "current-user":settings.CURRENT_USER
-            }
+                    "current-user":settings.CURRENT_USER
+                }
             return render(request, 'account.html', context=contex )
-    if request.method == 'POST' and 'reg-app' in request.POST:
-        client = Clients()
-        client.login = request.POST["login"]
-        client.email = request.POST["email"]
-        client.phone = request.POST["phone number"]
-        client.password = request.POST["password"]
-        client.name = request.POST["username"]
-        client.save()
-        
-    return render(request, 'auth.html')
+        if request.method == 'POST' and 'login-app' in request.POST:
+            
+            user = request.POST['login']
+            password = request.POST['password']
+            if Clients.objects.filter(login=user, password=password).first():
+                settings.CURRENT_USER = Clients.objects.filter(name=user, password=password).first()
+                contex = {
+                    "current-user":settings.CURRENT_USER
+                }
+                return render(request, 'account.html', context=contex )
+        if request.method == 'POST' and 'reg-app' in request.POST:
+            client = Clients()
+            client.login = request.POST["login"]
+            client.email = request.POST["email"]
+            client.phone = request.POST["phone number"]
+            client.password = request.POST["password"]
+            client.name = request.POST["username"]
+            client.save()
+    except:
+        return render(request, 'auth.html')
 
 
 def services(request):
